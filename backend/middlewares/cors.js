c4d45cfea915fc8ebceaa56c5d80ef72aa3229d2
cookiepatov <cookiepatov@gmail.com>
2021-07-09 12:21:50 +0300

    дополнить CORS

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -4,15 +4,20 @@ const allowedCors = [
   'localhost:3000',
 ];
 
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
 module.exports = (req, res, next) => {
   const { method } = req;
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.status(200).send();
   }
   const requestHeaders = req.headers['access-control-request-headers'];
   if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.status(200).send();
   }
 
   next();
