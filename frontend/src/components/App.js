cdf9cfce0b7980b899d92bb2b206a7d77f040e20
cookiepatov <cookiepatov@gmail.com>
2021-07-09 16:37:19 +0300

    Исправить ошибки формата данных

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -131,7 +131,7 @@ function App() {
 
 
   function handleCardLike(card) {
-    const isLiked = card.likes.some(like => like._id === currentUser._id);
+    const isLiked = card.likes.some(likeId => likeId === currentUser._id);
     api.changeLikeCardStatus(card._id, !isLiked)
       .then((newCard) => {
         setCards(cards => cards
