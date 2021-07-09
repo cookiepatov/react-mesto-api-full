a08e9d32e98f91dcf4a6bac06bcfe2b9a5b2cb09
cookiepatov <cookiepatov@gmail.com>
2021-07-09 12:32:43 +0300

    дополнить CORS

--- backend/app.js
+++ backend/app.js
@@ -44,11 +44,13 @@ app.use((req, res, next) => {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.status(200).send();
   }
   const requestHeaders = req.headers['access-control-request-headers'];
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.status(200).send();
   }
 
   next();
