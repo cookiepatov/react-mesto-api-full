360819b05c0a3075769569992233002041f3e07c
cookiepatov <cookiepatov@gmail.com>
2021-07-11 17:49:32 +0300

    Исправить формат выдачи ошибок на сервере

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -2,5 +2,5 @@
 module.exports = (err, req, res, next) => {
   const status = err.statusCode || 500;
   const { message } = err;
-  res.status(status).json({ err: message || 'На сервере произошла ошибка' });
+  res.status(status).json({ message: message || 'На сервере произошла ошибка' });
 };
